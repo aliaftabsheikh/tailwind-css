31496bceb34ea3b33b8cb3cd2cc26daef85ff7c9
aliaftabsheikh <80570243+aliaftabsheikh@users.noreply.github.com>
2023-02-21 16:51:29 +0500

    assets and setup project

---
+++ app/page.jsx
@@ -0,0 +1,29 @@
+import Image from 'next/image'
+import { Inter } from '@next/font/google'
+import styles from './page.module.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+
+    //! Lesson 01
+
+    // <div className="bg-emerald-500 w-52 h-52 rounded-full shadow-lg grid place-content-center drop-shadow-2xl radial-blue">
+    //   <div className='w-44 h-44 bg-red-600 rounded-full shadow-lg grid place-content-center'>
+    //     <div className='w-32 h-32 bg-blue-800 rounded-full shadow-lg grid place-content-center'>
+    //       <div className='w-20 h-20 bg-yellow-400 rounded-full shadow-lg grid place-content-center'>
+    //         <div className='w-12 h-12 bg-black rounded-full'></div>
+    //       </div>
+    //     </div>
+    //   </div>
+    // </div>
+
+
+    //! Lesson 2
+
+    <>
+   
+    </>
+      )
+}
