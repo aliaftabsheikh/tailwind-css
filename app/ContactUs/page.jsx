a7c2e7b4e41acca57f7750483fe51ff09732ac5f
aliaftabsheikh <80570243+aliaftabsheikh@users.noreply.github.com>
2023-02-25 09:05:18 +0500

    Contact us complete

--- app/ContactUs/page.jsx
+++ app/ContactUs/page.jsx
@@ -8,15 +8,24 @@ function Contact() {
           Contact Us
         </h2>
 
-<form action="">
+        <form
+          action=""
+          className="max-w-4xl mx-auto text-2xl sm:text-3xl flex flex-col items-left gap-4 "
+        >
+          <label for="subject">Subject:</label>
+          <input type="text" id="subject" name="subject" required minLength="3" maxLength="60" placeholder="Your Subject" className="w-full text-black text-2xl sm:text-3xl p-3 rounded-xl border border-solid border-slate-900 dark:border-none" />
 
-</form>
+          <label for="message">Message:</label>
 
+          <textarea name="message" id="message" cols="30" rows="10" placeholder="Your Message" required className="w-full text-black text-2xl sm:text-3xl p-3 rounded-xl border border-solid border-slate-900 dark:border-none"></textarea>
+
+          <button className="bg-teal-700 hover:bg-teal-600 active:bg-teal-500 text-white p-3 w-48 rounded-xl border border-solid border-slate-900 dark:border-none">Submit</button>
+
+        </form>
       </section>
 
       <hr className="mx-auto bg-black dark:bg-white w-1/2" />
     </>
-
   );
 }
 
